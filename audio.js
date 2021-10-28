6a84c7c764a0b7582ebaf98a81e0e6e2264e5897
Kishan Mandora <kishanmandora7@gmail.com>
2021-10-28 20:57:12 +0530

    feat: add audio using JS Audio constructor

---
+++ audio.js
@@ -0,0 +1,6 @@
+const myMusic = new Audio(
+	"./assets /One Piece OST - Very Very Strongest (Hip Hop _ Trap Remix) Prod.Cloud Tetsuo ( 256kbps cbr ).mp3"
+);
+
+document.addEventListener("click", () => myMusic.play());
+document.addEventListener("dblclick", () => myMusic.pause());
